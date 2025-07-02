bbdfb4a564dc848453e4fe27e17430b512128cc9
Abhishek Arora <154364538+AbhishekArora2105@users.noreply.github.com>
2025-07-02 20:50:14 +0000

    DataTypes Done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId = 10909
+let accountEmail = "abhi@gmail.com"
+var accountPass = "A12345"
+accountCity = "Jaipur"
+let accountABC;
+
+// accountId = 2 not allowed
+
+accountEmail = "hn@.com"
+console.log(accountEmail);
+console.table([accountEmail, accountId , accountPass , accountCity , accountABC])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")  we are using node js nt browser so alert will not work.
+
+// console.log(3+3)
+// console.log("Hello");
+
+let name = "abhi"
+let age = 18
+let isLoggedIn = false
+
+// number, bigInt , string , boolean , null , undefined , symbol
+
+//object
+
+console.log(typeof 90);
