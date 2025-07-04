8672050efe22988c2b2041455543c7182bd773be
Abhishek Arora <154364538+AbhishekArora2105@users.noreply.github.com>
2025-07-04 13:33:27 +0000

    Math library Done

---
+++ 01_Basics/04_nums_and_maths.js
@@ -0,0 +1,36 @@
+// const score = 400
+
+// const balance = new Number(100);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2)); // to define exponential degreem0.00
+
+// const num = 23.89666
+
+// console.log(num.toPrecision(1));
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6))
+
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(4,3,2,1,5,6));
+// console.log(Math.max(4,3,2,6,9,0));
+
+const min = 10
+const max = 20
+
+console.log(Math.random()*10+1)
+
+console.log(Math.random()*10+1)
+
+console.log(Math.floor(Math.random()*(max-min + 1) )+min)
+
+console.log(Math.floor(Math.random()*(max-min + 1) )+min)
+
