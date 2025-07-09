076298f6d1e2cebf9b8d9bb95932d4f87e1f2497
Abhishek Arora <154364538+AbhishekArora2105@users.noreply.github.com>
2025-07-09 08:37:18 +0000

    Date and Time done

---
+++ 01_Basics/05_Date.js
@@ -0,0 +1,19 @@
+//Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2023 , 0 , 23)
+// let myCreatedDate = new Date(2023 , 0 , 23 ,5 , 3)
+let myCreatedDate = new Date("2023-01-14")
+
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getDay());
+
