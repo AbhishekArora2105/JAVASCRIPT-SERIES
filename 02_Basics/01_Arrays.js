721510b06c4b56d6edcac711d8d193e99ca7b1ac
Abhishek Arora <154364538+AbhishekArora2105@users.noreply.github.com>
2025-07-10 11:45:38 +0000

    Arrays Done

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,37 @@
+//Array Methods
+
+const arr = [1,2,3,4,5]
+
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+// arr.unshift(9) add in begining.
+// arr.shift()  delete from begining.
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(3));
+
+// const newarr = arr.join()  //converts an array to string.
+
+//slice returns a block of elements from the array.
+
+//splice() deletes a range from array and cange it also.
+
+//concat() concvatenates two arrays and give single result in the form of a new array.
+
+// const a = [1,2,3]
+// const b = [4,5,6]
+// const c = [...a , ...b]
+// console.log(c);
+
+// const anrr = [1,2,3,[4,5,6],7,[6,7,8],9,0]
+
+// const real = anrr.flat(Infinity)
+
+// console.log(real);
+
+
+
+
+
+// console.log(arr)
