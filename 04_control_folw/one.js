0223de0922e4b63b709c2a2ed70dff7ba57f0674
Abhishek Arora <154364538+AbhishekArora2105@users.noreply.github.com>
2025-08-15 07:00:58 +0000

    Control flow done

---
+++ 04_control_folw/one.js
@@ -0,0 +1,42 @@
+//if
+const isuserloggedIn = true
+
+// if(isuserloggedIn){
+//     console.log(`Hello abhi`);
+    
+// }else{
+//     console.log(`not allowed`);
+    
+// }
+
+//short form
+
+// const balance = 1000
+
+// // if(balance>500) console.log("test");
+
+// if(balance <500){
+//     console.log("less than 500");
+// }else if (balance < 750){
+//     console.log("less than 750");
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if( userLoggedIn && debitCard){
+    console.log("Allow to buy course")
+}else{
+    console.log("Not Allowed");
+    
+}
+
+
+//ternerary operator
+
+const price = 100
+
+price >=80 ? console.log("yes") : console.log("no")
